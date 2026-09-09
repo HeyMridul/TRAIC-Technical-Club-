5a9b12fdf08ca64cb7be3ac4806dd2d34fffd6e6
Mridul Bhardwaj <mridul.2005.05@gmail.com>
2026-09-09 22:00:06 +0530

    joining section created

---
+++ src/app/join/page.tsx
@@ -0,0 +1,11 @@
+import { JoinSection } from "@/components/sections/JoinSection";
+
+export const metadata = { title: "Join TRAIC" };
+
+export default function JoinPage() {
+  return (
+    <div className="pt-16">
+      <JoinSection />
+    </div>
+  );
+}
